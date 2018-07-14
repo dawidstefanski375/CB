22e802cb0d8b93a37b55df868231455359388073
dawidstefanski375 <41240591+dawidstefanski375@users.noreply.github.com>
2018-07-14 21:03:56 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1366,5 +1366,5 @@
 	settings.intervalMessages.push('Join us on discord ! '+settings.discordLink);
 	localStorage.setItem('basicBotsettings', JSON.stringify(settings));
 
-	$.getScript("https://rawgit.comdawidstefanski375/CB/master/Blex-Bot.js", extend);
+	$.getScript("https://rawgit.com/dawidstefanski375/CB/master/Blex-Bot.js", extend);
 }).call(this);
