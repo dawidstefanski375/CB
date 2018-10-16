b1a3687084c90664bade029000cbdb5d32f30e21
dawidstefanski375 <41240591+dawidstefanski375@users.noreply.github.com>
2018-10-16 19:26:25 +0200

    Update extensionn.js

--- extensionn.js
+++ extensionn.js
@@ -1500,7 +1500,7 @@
 		website: "https://goo.gl/azYtb9",
 		intervalMessages: [],
 		messageInterval: 0,
-		songstats: false,
+		songstats: true,
 		commandLiteral: "!",
 		blacklists: {
 			NSFW: null,
