b32aeb34638113700d7bb0be29b1ff99f3489aa1
dawidstefanski375 <41240591+dawidstefanski375@users.noreply.github.com>
2019-01-06 05:24:28 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2431,23 +2431,7 @@
                     }
                 }
             },
-         
-            discordCommand: {
-                command: 'discord',
-                rank: 'user',
-                type: 'exact',
-                functionality: function(chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void(0);
-                    else {
-                        if (typeof basicBot.settings.discordLink === 'string')
-                            API.sendChat(subChat(basicBot.chat.discord, {
-                                link: basicBot.settings.discordLink
-                            }));
-                    }
-                }
-            },         
-         
+                 
             dcinfoCommand: {
                 command: 'dcinfo',
                 rank: 'user',
