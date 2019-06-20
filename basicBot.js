44de68419b0dcdce8292e89558ddcab54e00fd7c
dawidstefanski375 <41240591+dawidstefanski375@users.noreply.github.com>
2019-06-20 16:00:31 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -327,7 +327,7 @@
             website: "https://goo.gl/GwPe67",
             intervalMessages: [],
             messageInterval: 5,
-            songstats: false,
+            songstats: true,
             commandLiteral: '!',
             blacklists: {
                 NSFW: 'https://raw.githack.com/dawidstefanski375/CB/master/blacklists/NSFWlist.json',
